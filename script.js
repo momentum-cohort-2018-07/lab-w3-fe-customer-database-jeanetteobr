cc111a943a45efc92800b20ed707a40941f796ec
jeanetteobr <jeanette.obr@gmail.com>
2018-08-15 14:47:13 -0400

    Initial commit: created customer class

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 console.log('Write your code here!')
 console.log('customers and moment are available to you.')
+
+class Customer {
+    constructor (name, email, location, dob, registered) {
+        this.name = name; 
+        this.email = email;
+        this.location = location;
+        this.dob = dob;
+        this.registered = registered;
+    }
+    getName = () => {
+        return name[1][2]
+    }
+    getLocation = ()=> {
+        return location
+    }
+    getBirthdate = () => {
+        return moment(dob).format('MMM Do, YYYY')
+    }
+    getRegDate = () => {
+        return moment(registered).format('MMM Do, YYYY')
+    }
+}
+
